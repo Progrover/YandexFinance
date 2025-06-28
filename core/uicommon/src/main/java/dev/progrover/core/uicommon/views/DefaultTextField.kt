65d8e2013b38678c35df51e4a9c1ef52aca85519
Progrover <msaharovforwork@gmail.com>
2025-06-29 01:57:46 +0300

    ARTICLES: реализован локальный поиск по статьям

---
+++ core/uicommon/src/main/java/dev/progrover/core/uicommon/views/DefaultTextField.kt
@@ -0,0 +1,104 @@
+package dev.progrover.core.uicommon.views
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import dev.progrover.core.theme.AppTheme
+import dev.progrover.core.uicommon.utils.conditionally
+
+@Composable
+fun DefaultTextField(
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .background(AppTheme.colors.containerHigh)
+        .padding(AppTheme.paddings.padding4),
+    enabled: Boolean = true,
+    text: String,
+    focusRequester: FocusRequester? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    @StringRes hintResId: Int,
+    @DrawableRes startIconId: Int? = null,
+    @DrawableRes endIconId: Int? = null,
+    onTextChange: (String) -> Unit,
+) {
+
+    BasicTextField(
+        modifier = modifier
+            .conditionally(
+                condition = (focusRequester != null),
+                trueExtension = {
+                    focusRequester(focusRequester!!)
+                }),
+        value = text,
+        enabled = enabled,
+        singleLine = true,
+        textStyle = AppTheme.typography.bodyLarge.copy(color = AppTheme.colors.textSecondary),
+        keyboardOptions = keyboardOptions,
+        onValueChange = { newText -> onTextChange(newText) },
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(AppTheme.paddings.padding4),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            if (startIconId != null) {
+                Image(
+                    modifier = Modifier
+                        .padding(AppTheme.paddings.padding16)
+                        .size(AppTheme.sizes.size24),
+                    imageVector = ImageVector.vectorResource(startIconId),
+                    contentDescription = null,
+                )
+            } else Spacer(Modifier.width(AppTheme.paddings.padding16))
+
+            Box(
+                modifier = Modifier.weight(1f)
+            ) {
+
+                if (text.isEmpty()) {
+                    Text(
+                        modifier = Modifier,
+                        text = stringResource(hintResId),
+                        maxLines = 1,
+                        style = AppTheme.typography.bodyLarge,
+                    )
+                }
+
+                it()
+            }
+
+            if (endIconId != null) {
+                Image(
+                    modifier = Modifier
+                        .padding(AppTheme.paddings.padding16)
+                        .size(AppTheme.sizes.size24),
+                    imageVector = ImageVector.vectorResource(endIconId),
+                    contentDescription = null,
+                )
+            }
+        }
+    }
+}
--- feature/articles/impl/src/main/java/dev/progrover/articles/impl/presentation/contract/articles/ArticlesUIState.kt
+++ feature/articles/impl/src/main/java/dev/progrover/articles/impl/presentation/contract/articles/ArticlesUIState.kt
@@ -3,11 +3,17 @@ package dev.progrover.articles.impl.presentation.contract.articles
 import dev.progrover.core.base.model.Category
 import dev.progrover.core.base.model.Error
 import dev.progrover.core.base.presentation.mvi.UIState
+
 /**
  * Класс, необходимый для отслеживания состояния articles feature
  */
 data class ArticlesUIState(
     val isLoading: Boolean = false,
-    val articles: List<Category> = emptyList(),
+    val searchText: String = "",
+    val allArticles: List<Category> = emptyList(),
+    // Тк у нас конечное количество статей => отображение без пагинации,
+    // приходится вводить дополнительную переменную для отображения
+    // отсортированного списка
+    val articlesForPresentation: List<Category> = emptyList(),
     val error: Error? = null,
 ) : UIState
